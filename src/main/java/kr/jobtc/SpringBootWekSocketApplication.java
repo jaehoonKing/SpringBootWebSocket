00bcd2e5971738426cde6410c98010685b5686f5
윤재훈 <jaehoon940901@gmail.com>
2023-04-17 18:02:51 +0900

    최초커밋

---
+++ src/main/java/kr/jobtc/SpringBootWekSocketApplication.java
@@ -0,0 +1,13 @@
+package kr.jobtc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWekSocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWekSocketApplication.class, args);
+	}
+
+}
